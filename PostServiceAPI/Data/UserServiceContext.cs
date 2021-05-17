7a956cfa3dc1f183bb84440ec6ed21ad683aab4e
ponvas <38868237+ponvas@users.noreply.github.com>
2021-05-17 01:11:29 -0500

    Add files via upload

---
+++ PostServiceAPI/Data/UserServiceContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PostServiceAPI.Entites;
+
+namespace PostServiceAPI.Data
+{
+   
+
+        public class PostServiceContext : DbContext
+        {
+            public PostServiceContext(DbContextOptions<PostServiceContext> options)
+                : base(options)
+            {
+            }
+
+            public DbSet<Post> Post { get; set; }
+            public DbSet<PostUser> User { get; set; }
+  
+    }
+}
---
+++ UserServiceAPI/Data/UserServiceContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using UserServiceAPI.Entites;
+
+namespace UserServiceAPI.Data
+{
+    public class UserServiceContext : DbContext
+    {
+        public UserServiceContext(DbContextOptions<UserServiceContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> User { get; set; }
+    }
+}
